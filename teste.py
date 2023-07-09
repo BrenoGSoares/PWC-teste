e6c93d6aae0642164956da0243d7d8087e963357
Breno <breno.givas@icloud.com>
2023-07-09 02:28:13 -0300

    Teste Estágio PWC

---
+++ teste.py
@@ -0,0 +1,29 @@
+'''
+1 - Reverta a ordem das palavras nas frases, mantendo a ordem das palavras.
+a. Input: "Hello, World! OpenAI is amazing"
+b. Output: "amazing. is OpenAi World! Hello,"
+'''
+
+'''
+2 - Remova todos os caracteres duplicados da string abaixo:
+a. Input: "Hello, World!"
+b. Output: "Helo, Wrd!"
+'''
+
+'''
+3 - Encontre a substring palindroma mais longa na string abaixo:
+a. Input: "babad"
+b. Output: "bab"
+'''
+
+'''
+4 - Coloque em maiúscula a primeira leitra de cada frase na string:
+a. Input "hello, how are you? i'm fine, thank you."
+b. Output "Hello, How are you? I'm fine, thank you."
+'''
+
+'''
+5 - Verifique se a string é um anagrama de um palindromo:
+a. Input: "racecar"
+b. Output: true
+'''
